4550ac6feeb79c51677e30f2de37627341f4cbfa
Ak56Ai <akchouhan131@gmail.com>
2026-02-28 16:23:56 +0530

    Updated supabaseClient.ts

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8cce160858589f1e"; }
+/******/ 		__webpack_require__.h = function() { return "7aeb8a2068b55716"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
